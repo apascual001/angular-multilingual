2a9af31430a6b1e54c59fa32e6255407ee71a6a1
Ana Pascual <annapascual001@gmail.com>
2020-03-25 17:35:39 +0100

    App module Imports de TranslateLoader, TranslateModule y TranslateHttpLoader: Corresponden a las módulos y providers necesarios para que ngx-translate funcione correctamente. Configuración de TranslateModule: Se debe utilizar un loader personalizado para este módulo. TranslateHttpLoader cargará archivos de idioma bajo demanda utilizando HttpClient.

--- tutorial-angular7-multilenguaje/src/app/app.module.ts
+++ tutorial-angular7-multilenguaje/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Translation
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslationComponent } from './translation/translation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [ HttpClient ]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
